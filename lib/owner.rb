c4c5846a1f3cf025417719d95f93924dc543acc5
Christopher Tompkins <chtompkins026@gmail.com>
2018-10-25 01:07:42 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -26,7 +26,7 @@ class Owner
   end 
   
   def walk_dogs
-    @species.select do |specie, mood| 
+    @species.collect do |specie, mood| 
       if specie == :dogs 
         mood.each do |mood|
           dog.mood = "happy"
